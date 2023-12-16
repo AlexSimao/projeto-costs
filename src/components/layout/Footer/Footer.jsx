5035da946e535a32010b949706592fdf264c015f
Alex Simão de Souza <126366771+AlexSimao@users.noreply.github.com>
2023-12-16 00:13:03 -0300

    Criando e estilizando o componente Footer

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,10 +1,36 @@
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
 import styles from "./Footer.module.css";
 
-function Footer(props) {
+function Footer() {
   return (
-    <footer>
-      <h1>Goodbye</h1>
-    </footer>
+    <>
+      <footer className={styles.footer}>
+        <div>
+          <a
+            href="https://www.facebook.com/alexssimao1/"
+            target="_blank"
+          >
+            <FaFacebook />
+          </a>
+          <a
+            href="https://www.instagram.com/alexssimao1/"
+            target="_blank"
+          >
+            <FaInstagram />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/alexss1/"
+            target="_blank"
+          >
+            <FaLinkedin />
+          </a>
+        </div>
+
+        <p>
+          Costs <span>&copy; 2023 </span>
+        </p>
+      </footer>
+    </>
   );
 }
 
