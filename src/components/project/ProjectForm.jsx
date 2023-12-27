90401c0c8a3d3768eda32d091de6169279abbed4
Alex Simão de Souza <126366771+AlexSimao@users.noreply.github.com>
2023-12-27 16:16:35 -0300

    Criando a base do ProjectForm. Que serve para criar ovos projetos.

---
+++ src/components/project/ProjectForm.jsx
@@ -0,0 +1,34 @@
+function ProjectForm() {
+  return (
+    <form method="post">
+      <div>
+        <input
+          type="text"
+          placeholder="Insira o nome do Projeto"
+        />
+      </div>
+      <div>
+        <input
+          type="number"
+          placeholder="Insira o orçamento total"
+        />
+      </div>
+      <div>
+        <select
+          name="category_id"
+          id="category_id"
+        >
+          <option disabled>Selecione uma categoria</option>
+        </select>
+      </div>
+      <div>
+        <input
+          type="submit"
+          value="Criar Projeto"
+        />
+      </div>
+    </form>
+  );
+}
+
+export default ProjectForm;
--- src/components/routers/NewProject.jsx
+++ src/components/routers/NewProject.jsx
@@ -1,8 +1,13 @@
+import ProjectForm from "../project/ProjectForm";
+import styles from "./NewProject.module.css"
+
 function NewProject() {
   return (
-    <>
-      <h1>NewProject</h1>
-    </>
+    <div className={styles.newproject_container}>
+      <h1>Criar Projeto</h1>
+      <p>Crie Seu projetos para dpois adicionar os serviços</p>
+      <ProjectForm />
+    </div>
   );
 }
 
